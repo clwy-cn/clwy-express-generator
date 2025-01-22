23eaeaa0fa1690050ed72ac624944c5dfea65d51
Dong Liu <clwy@clwy.cn>
2025-01-23 00:53:59 +0800

    fix error-handle

--- templates/mjs/middlewares/error-handler.js
+++ templates/mjs/middlewares/error-handler.js
@@ -1,6 +1,6 @@
 // middlewares/error-handler.js
 
-module.exports = (err, req, res, next) => {
+export default (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
